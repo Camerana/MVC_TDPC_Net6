53ba349488f1bb5ec437104e2e2714a2fe6a9322
trippone trippini <visualtrippa@hotmail.com>
2023-06-27 14:39:41 +0200

    master branch 100%

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MVC_TDPC_Net6.Controllers
         {
             return View();
         }
+        public IActionResult ButtonPage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
